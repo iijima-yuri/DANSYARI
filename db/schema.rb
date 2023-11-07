074e35ff1e2ad21cf9a094b3b67e47619c58b733
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2023-11-07 10:56:32 +0900

    model,dbの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_31_183420) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_06_063018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "episode_content", null: false
+    t.text "reason_content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_31_183420) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
