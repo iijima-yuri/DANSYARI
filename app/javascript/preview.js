f749c3cf7d227fc256b0c619ee6034d4ee073eb0
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2023-11-14 16:28:54 +0900

    画像プレビュー（修正）

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,18 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const elements = document.querySelectorAll('.js-file-select-preview')
-  if (!elements) return
-
-  elements.forEach((element) => {
-    const previewElement = document.querySelector(element.CDATA_SECTION_NODE.target)
-    element.addEventListener('change', (e) => {
-      const reader = new FileReader();
-      ReadableStreamDefaultReader.onloaded = () => {
-        previewElement.src = reader.result;
+function previewImage() {
+  const target = this.event.target;
+  const file = target.files[0];
+  const reader  = new FileReader();
+  reader.onloadend = function () {
+      const preview = document.querySelector("#preview")
+      if(preview) {
+          preview.src = reader.result;
       }
-      const file = e.target.files[0]
-      if (file) {
-        reader.readAsDataURL(file);
-      }
-    })
-  })
-})
+  }
+  if (file) {
+      reader.readAsDataURL(file);
+  }
+}
