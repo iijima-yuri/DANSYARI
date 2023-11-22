4f9722d16128cd38fde63e5f14d2b0571dbfdb8c
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2023-11-22 21:43:24 +0900

    dbの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_17_050333) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_063603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_17_050333) do
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "commentable_type", default: " ", null: false
+    t.bigint "commentable_id", default: 0, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["item_id"], name: "index_comments_on_item_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
