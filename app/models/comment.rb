c0666dbb1b57b5a89bd9e1a990d2d3e831c88c32
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2023-11-23 21:53:00 +0900

    各モデルの関連づけ

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  belongs_to :commentable, polymorphic: true
 
   validates :body, presence: true, length: { maximum: 56_535 }
 end
