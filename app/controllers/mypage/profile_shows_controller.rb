8ed2a637a4a30d2ab9ca88ccc14bf19581865a0c
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2023-12-21 05:37:11 +0900

    コントローラの追加

--- app/controllers/mypage/profile_shows_controller.rb
+++ app/controllers/mypage/profile_shows_controller.rb
@@ -1,5 +1,7 @@
 class Mypage::ProfileShowsController < Mypage::BaseController
   def show
     @user = User.find(params[:id])
+    @following_users = @user.following_users
+    @follower_users = @user.follower_users
   end
 end
