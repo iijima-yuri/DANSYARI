94acc158e70fcce4bd662dd6d8165f63f0c6659e
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2023-12-24 15:25:25 +0900

    rubocopの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,27 +22,24 @@ class Item < ApplicationRecord
 
   def create_notification_favorite(current_user)
     temp = Notification.where(["visitor_id = ? and visited_id = ? and item_id = ? and action = ? ", current_user.id, user_id, id, 'favorite'])
-    if temp.blank?
-      notification = current_user.active_notifications.new(item_id: id, visited_id: user_id, action: 'favorite')
-      if notification.visitor_id == notification.visited_id
-        notification.checked = true
-      end
-      notification.save if notification.valid?
-    end
+    return unless temp.blank?
+
+    notification = current_user.active_notifications.new(item_id: id, visited_id: user_id, action: 'favorite')
+    notification.checked = true if notification.visitor_id == notification.visited_id
+
+    notification.save if notification.valid?
   end
 
   def create_notification_comment(current_user, comment_id, item_id)
     item = Item.find(item_id)
-    if item && current_user != item.user
-      save_notification_comment(current_user, comment_id, item.user.id)
-    end
+    return unless item && current_user != item.user
+
+    save_notification_comment(current_user, comment_id, item.user.id)
   end
 
   def save_notification_comment(current_user, comment_id, visited_id)
-    notification = current_user.active_notifications.new(item_id: id, comment_id: comment_id, visited_id: visited_id, action: 'comment')
-    if notification.visitor_id == notification.visited_id
-      notification.checked = true
-    end
+    notification = current_user.active_notifications.new(item_id: id, comment_id:, visited_id:, action: 'comment')
+    notification.checked = true if notification.visitor_id == notification.visited_id
     notification.save if notification.valid?
   end
 
