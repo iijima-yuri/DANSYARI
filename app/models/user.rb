3338fabf680c0ef15e646899bea606a77048a4d3
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2023-12-27 21:11:34 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@ class User < ApplicationRecord
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
+  has_many :entries
+  has_many :messages
+  has_many :chat_rooms, through: :entries
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
