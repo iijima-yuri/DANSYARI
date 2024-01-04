348801b61f50a38e5c6f399688be730fee04ee3a
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2024-01-04 20:49:20 +0900

    jobの作成・編集

---
+++ app/jobs/message_broadcast_job.rb
@@ -0,0 +1,13 @@
+class MessageBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+    ActionCable.server.broadcast "room_channel_#{message.chat_room_id}", { message: render_message(message) }
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.render(partial: 'chat_rooms/message', locals: { message: message })
+  end
+end
