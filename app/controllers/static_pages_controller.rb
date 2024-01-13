a4c62d7a110b421ce038c64b275ca0355cff9acd
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2024-01-13 22:19:09 +0900

    コントローラへの追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: [:top]
+  skip_before_action :require_login
   def top
     @q = Item.ransack(params[:q])
   end
+
+  def terms_of_service; end
+
+  def privacy_policy; end
 end
