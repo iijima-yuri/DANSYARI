e20c30cc72f0288306b0199d35f6f426eb75ed62
iijima-yuri <y_mjkss_a.m.com.or.jp@docomo.ne.jp>
2024-01-25 10:59:34 +0900

    チャットの通知の雛形作成

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,24 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
 
+  has_many :notifications, dependent: :destroy
+
   after_create_commit { MessageBroadcastJob.perform_later self }
+
+  def create_notification_chat(current_user, message_id, room_id, visited_id)
+    temp_ids = Message.select(:user_id).where(room_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_chat(current_user, message_id, temp_id['user_id'])
+    end
+    save_notification_chat(current_user, message_id, visited_id) if temp_ids.blank?
+  end
+
+  def save_notification_chat(current_user, message_id, visited_id)
+    notification = current_user.active_notifications.new(message_id: id, visited_id: visited_id, action: 'chat')
+
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_20_100933) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_24_173731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_20_100933) do
     t.datetime "updated_at", null: false
     t.string "commentable_type", default: " ", null: false
     t.bigint "commentable_id", default: 0, null: false
-    t.index %w[commentable_type commentable_id], name: "index_comments_on_commentable"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["item_id"], name: "index_comments_on_item_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_20_100933) do
     t.bigint "tag_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index %w[item_id tag_id], name: "index_item_tags_on_item_id_and_tag_id", unique: true
+    t.index ["item_id", "tag_id"], name: "index_item_tags_on_item_id_and_tag_id", unique: true
     t.index ["item_id"], name: "index_item_tags_on_item_id"
     t.index ["tag_id"], name: "index_item_tags_on_tag_id"
   end
@@ -110,6 +110,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_20_100933) do
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "message_id"
     t.index ["comment_id"], name: "index_notifications_on_comment_id"
     t.index ["item_id"], name: "index_notifications_on_item_id"
     t.index ["visited_id"], name: "index_notifications_on_visited_id"
